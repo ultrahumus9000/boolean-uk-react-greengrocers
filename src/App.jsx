3c1762e55a7a4371ae1ef393f8122a4dd3759dde
Linlin Li <paris1900lll@gmail.com>
2021-06-14 16:20:59 +0100

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Main from "./components/Main";
 import Header from "./components/Header";
-import inititalStoreFoods from "./storeinfo"
+// import inititalStoreFoods from "./storeinfo"
 
 import "./styles/index.css";
 
 
 export default function App() {
 
-const [products, setProducts]= useState(inititalStoreFoods)
+const [products, setProducts]= useState([])
 const [cartItems, setCartItems] = useState([])
+
 // const [totalPrice, setTotalPrice] = useState(0.0); because we can just calculate it 
+
+
+useEffect(()=>{},[])
 
   return <div className="App">
     <Header products={products} setProducts = {setProducts} cartItems={cartItems} setCartItems ={setCartItems} />
